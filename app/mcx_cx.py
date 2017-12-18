125e8304276d793a51128c81b12bfa3c50db5bec
winsert <winsert@163.com>
2017-12-18 11:14:12 +0800

    修改dqjz的y值

--- app/mcx_cx.py
+++ app/mcx_cx.py
@@ -69,6 +69,8 @@ def getDQJZ(synx, shj,  ll):
     inty = int(y)
     if y > inty: 
         y = inty + 1
+    else:
+        y = inty
 
     l = mnlv.split(',') #转成列表
     for i in range (len(l)-y, len(l)-1):
