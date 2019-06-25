109bec428a7d8779747965ce1f57f43b947d58dc
winsert <winsert@163.com>
2019-06-25 09:02:18 +0800

    to tab:cb0

--- app/wxcx.py
+++ app/wxcx.py
@@ -87,7 +87,7 @@ def getWXCX():
     try:
         conn = sqlite3.connect('cb.db')
         curs = conn.cursor()
-        sql = "select name, Code, zgcode, Prefix, jian, jia, zhong, Note, zgj, hsqsr, hsj, dqr, position, shj, ll, ce, qs, qss, zgqsr from cb ORDER BY Code"
+        sql = "select name, Code, zgcode, Prefix, jian, jia, zhong, Note, zgj, hsqsr, hsj, dqr, position, shj, ll, ce, qs, qss, zgqsr from cb0 ORDER BY Code"
         curs.execute(sql)
         tmp = curs.fetchall()
         curs.close()
